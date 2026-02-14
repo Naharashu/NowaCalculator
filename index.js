a67701adfe2e2679aee2efb35f41c534baa47045
Naharashu <130159705+Naharashu@users.noreply.github.com>
2026-02-14 22:05:05 +0200

    updating to 1.1.0

--- index.js
+++ index.js
@@ -4,18 +4,41 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 400,
     height: 245,
-    autoHideMenuBar: true,
-    title: "NowaCalculator v1.0.0"
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webviewTag: false,
+      enableWebSQL: false,
+      spellcheck: false,
+      sandbox: true,
+      plugins: false,
+      webSecurity: true,
+      webgl: false
+    },
+    autoHideMenuBar: true
   })
 
-  win.setTitle("NowaCalculator v1.0.0")
+  win.setTitle("NowaCalculator v1.1.0")
   win.loadFile('index.html');
 }
+
+app.disableHardwareAcceleration();
+
+app.commandLine.appendSwitch('disable-background-update');
+app.commandLine.appendSwitch('disable-geolocation');
+app.commandLine.appendSwitch('disable-extensions-update');
+app.commandLine.appendSwitch('disable-features', 'AutofillEnableProfileAutofill,AutofillEnableAddressAutofill');
+app.commandLine.appendSwitch('js-flags', '--max-old-space-size=128');
+app.commandLine.appendSwitch('disable-software-rasterizer');
+app.commandLine.appendSwitch('disable-sync');
+app.commandLine.appendSwitch('disable-print-preview');
+app.commandLine.appendSwitch('disable-speech-api');
+
 
 app.on("ready", () => {
   createWindow();
 });
 
-app.on("quit", () => {
-    app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
